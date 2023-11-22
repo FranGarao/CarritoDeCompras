6852369c5970f5f17e2fd6eb1ee77ac2ae315214
frangarao <garaofran@gmail.com>
2023-11-22 18:29:30 -0300

    agregue un sistema de filtrado de productos, ademas de agregar nuevos productos

--- assets/app-original.js
+++ assets/app-original.js
@@ -1,26 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
   let carrito = [];
-
   const numeroCarrito = document.querySelector(".numeroCarrito");
   const botonCarrito = document.getElementById("carrito");
   const verCarrito = document.getElementById("verCarrito");
   const verPrecioFinal = document.getElementById("verPrecioFinal");
   const filtros = document.getElementById("filtros");
+  const precioAscendente = document.getElementById("precioAscendente");
+  const precioDescendente = document.getElementById("precioDescendente");
+  const precioPersonalizado = document.getElementById("precioPersonalizado");
+  const mostrarFiltros = document.querySelectorAll(".listaFiltros");
+  let activarFiltros = false;
   let talles = [
     ["S", "M", "L", "XL", "XXL"],
     [39, 40, 41, 42, 43, 44],
   ];
   let productos = [
     {
+      nombre: "producto-a",
       marca: "Nike",
       tipo: "Zapatillas",
-      precio: 65000,
+      precio: 95000,
       talle: talles[1],
       importado: true,
       stock: 3,
     },
     {
-      marca: "Adidas",
+      nombre: "producto-b",
+      marca: "Nike",
       tipo: "Buzo",
       precio: 40000,
       talle: talles[0],
@@ -28,19 +34,74 @@ document.addEventListener("DOMContentLoaded", function () {
       stock: 2,
     },
     {
+      nombre: "producto-c",
       marca: "Jordan",
       tipo: "Remera",
-      precio: 35000,
+      precio: 23000,
       talle: talles[0],
       importado: true,
       stock: 2,
     },
+    {
+      nombre: "producto-d",
+      marca: "Jordan",
+      tipo: "Buzo",
+      precio: 38000,
+      talle: talles[0],
+      importado: true,
+      stock: 2,
+    },
+    {
+      nombre: "producto-e",
+      marca: "Jordan",
+      tipo: "Short",
+      precio: 22000,
+      talle: talles[0],
+      importado: true,
+      stock: 2,
+    },
+    {
+      nombre: "producto-f",
+      marca: "Jordan",
+      tipo: "Zapatillas",
+      precio: 110000,
+      talle: talles[1],
+      importado: true,
+      stock: 10,
+    },
+    // {
+    //   nombre: "producto-a",
+    //   marca: "Jordan",
+    //   tipo: "Zapatillas",
+    //   precio: 85000,
+    //   talle: talles[1],
+    //   importado: true,
+    //   stock: 10,
+    // },
+    // {
+    //   nombre: "producto-a",
+    //   marca: "Nike",
+    //   tipo: "Remera",
+    //   precio: 25000,
+    //   talle: talles[0],
+    //   importado: true,
+    //   stock: 2,
+    // },
+    // {
+    //   nombre: "producto-a",
+    //   marca: "Nike",
+    //   tipo: "Buzo",
+    //   precio: 35000,
+    //   talle: talles[0],
+    //   importado: true,
+    //   stock: 2,
+    // },
   ];
 
   console.log(productos);
 
   productos.map((el) => {
-    const agregarProducto = document.getElementById(`${el.marca}`);
+    const agregarProducto = document.getElementById(`${el.nombre}`);
     agregarProducto.addEventListener("click", () => {
       if (el.stock > 0) {
         const productoEnCarrito = carrito.find(
@@ -94,56 +155,69 @@ document.addEventListener("DOMContentLoaded", function () {
   console.log(carrito);
 
   filtros.addEventListener("click", () => {
-    const filtroUsuario = parseInt(
+    mostrarFiltros.forEach((el) => {
+      if (activarFiltros == true) {
+        // alert("El resultado de los filtros se muestra en consola.");
+        el.classList.remove("desaparecerFiltros");
+      } else {
+        el.classList.add("desaparecerFiltros");
+      }
+    });
+    activarFiltros = !activarFiltros;
+  });
+
+  precioAscendente.addEventListener("click", () => {
+    const mostrarPrecioAscendente = productos.sort(
+      (a, b) => a.precio - b.precio
+    );
+    console.log(mostrarPrecioAscendente);
+  });
+  precioDescendente.addEventListener("click", () => {
+    const mostrarPrecioDescendente = productos.sort(
+      (a, b) => b.precio - a.precio
+    );
+    console.log(mostrarPrecioDescendente);
+  });
+
+  precioPersonalizado.addEventListener("click", () => {
+    const opcionUsuario = parseInt(
       prompt(
-        "Que filtro desea aplicar?\n\n1) Filtro descendente\n2) Filtro Ascendente\n3) Filtrar por precio personalizado"
+        "Elegi una opcion:\nProductos con precio menor a...(oprimi 1)\nProductos con precio mayor a...(oprimi 2)"
       )
     );
-    switch (filtroUsuario) {
-      case 1:
-        const filtroDescendente = productos.sort((a, b) => b.precio - a.precio);
-        console.log(filtroDescendente);
-        break;
-      case 2:
-        const filtroAscendente = productos.sort((a, b) => a.precio - b.precio);
-        console.log(filtroAscendente);
-        break;
-      default:
-        break;
+    if (opcionUsuario == 1) {
+      const precioUsuario = parseInt(prompt("Ingresa el precio maximo"));
+      const filtroPersonalizado = productos.filter((el) => {
+        return el.precio < precioUsuario;
+      });
+      if (filtroPersonalizado.length == 0) {
+        alert("No se encontraron resultados.");
+      } else {
+        console.log(filtroPersonalizado);
+      }
+    } else if (opcionUsuario == 2) {
+      const precioUsuario = parseInt(prompt("Ingresa el precio minimo"));
+      const filtroPersonalizado = productos.filter((el) => {
+        return el.precio >= precioUsuario;
+      });
+      if (filtroPersonalizado.length == 0) {
+        alert("No se encontraron resultados.");
+      } else {
+        console.log(filtroPersonalizado);
+      }
+    } else {
+      alert("Por favor, ingresa 1 o 2.");
     }
-    const filtroMenor = productos.filter((el) => el.precio < filtroUsuario);
-    const filtroMayor = productos.filter((el) => el.precio > filtroUsuario);
-    const filtroAscendente = productos.sort((a, b) => b.precio - a.precio);
   });
 });
 
-// Filtros.
-// const precioBajo = carrito.filter((el) => el.precio < 80000);
-// const importado = carrito.filter((el) => el.importado === true);
-// console.log(precioBajo);
-// console.log(importado);
-
-// Realize una actualizacion de los precios en un 10%.
-// const actualizado = carrito.map((el) => {
-//   return {
-//     nombre: el.nombre,
-//     precio: el.precio + el.precio / 0.1,
-//   };
+// precioPersonalizado.addEventListener("click", () => {
+//   prompt(
+//     "Ingresa el precio maximo, y te mostramos los productos por debajo de dicho precio."
+//   );
+//   const precioUsuario = parseInt(prompt("Ingrese el precio minimo"));
+//   const productosFiltrados = productos.filter((el) => {
+//     return el.precio > precioUsuario;
+//   });
+//   console.log(productosFiltrados);
 // });
-
-// console.log(actualizado);
-//   verCarrito.innerHTML = `<h2>${prod.tipo} ${prod.marca} $${prod.precio} Talles disponibles: ${tallasDisponibles}</h2>`;
-//   botonCarrito.disabled = true;
-// }
-// mostrarCarrito.addEventListener("click", () => {
-//   for (const prod of carrito) {
-//     let verCarrito = document.createElement("div");
-//     const carritoElement = document.getElementById("carrito");
-//     if (carritoElement) {
-//       carritoElement.appendChild(verCarrito);
-//       verCarrito.innerHTML = `<h2>${prod.marca}</h2>`;
-//     } else {
-//       console.error("No se encontró el elemento con ID 'carrito'");
-//     }
-//   }
-// })
